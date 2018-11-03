0068418434ab26b6fb1488e1dbe83c4e89a51009
Techbear <techbearHub@users.noreply.github.com>
2018-11-03 21:56:28 +0200

    Added new classes Added button

---
+++ FileSync/Tools/filereader.cpp
@@ -0,0 +1,6 @@
+#include "filereader.h"
+
+Filereader::Filereader()
+{
+
+}
---
+++ FileSync/Tools/filereader.h
@@ -0,0 +1,12 @@
+#ifndef FILEREADER_H
+#define FILEREADER_H
+
+#include <QWidget>
+
+class Filereader
+{
+public:
+    Filereader();
+};
+
+#endif // FILEREADER_H
